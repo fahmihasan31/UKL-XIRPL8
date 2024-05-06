4884bae855d37a87146b8ed460d446f2c9015725
Fahmi Hasan <mochammadfahmihasan@gmail.com>
2024-05-06 20:11:33 +0700

    first commit

---
+++ models/order_list.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class order_list extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  order_list.init({
+    orderList_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    customer_name: DataTypes.STRING,
+    order_type: DataTypes.STRING,
+    order_date: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'order_list',
+  });
+  return order_list;
+};
